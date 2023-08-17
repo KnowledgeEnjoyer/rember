d8b82f173164263012b0a85da4831179ba4c5ee4
KnowledgeEnjoyer <ghsidghisdahfisdhifs@ghdisag.com>
2023-08-17 17:29:42 -0300

    Assigning numeric sequential ids to created decks.

--- rember.bash
+++ rember.bash
@@ -2,9 +2,28 @@
 
 
 export REMBER_CONFIG_FOLDER="$HOME/.config/rember"
+export REMBER_CONFIG_FILE_NAME=".rember.yaml"
 
 create_config_folder() {
     mkdir -p "$REMBER_CONFIG_FOLDER"
+
+    if [ ! -f "$REMBER_CONFIG_FOLDER/"$REMBER_CONFIG_FILE_NAME"" ]; then
+
+cat <<- EOF > "$REMBER_CONFIG_FOLDER/"$REMBER_CONFIG_FILE_NAME""
+last_id_created: 0
+EOF
+
+    fi
+}
+
+generate_deck_id() {
+
+    local last_id_created=$(yq '.last_id_created' $REMBER_CONFIG_FOLDER/"$REMBER_CONFIG_FILE_NAME")
+    local new_id=$(( last_id_created+=1 ))
+
+    yq -iy ".last_id_created = $new_id" "$REMBER_CONFIG_FOLDER/"$REMBER_CONFIG_FILE_NAME""
+    
+    echo $new_id
 }
 
 create_deck() {
@@ -12,11 +31,11 @@ create_deck() {
     subject_slug=$(echo "$1" | tr 'A-Z' 'a-z' | sed s/\ /_/g)
 
 cat <<- EOF > ${REMBER_CONFIG_FOLDER}/${subject_slug}.yaml
+id: $(generate_deck_id)
 subject: $1
 cards:
 EOF
 
-    return $?
 }
 
 
