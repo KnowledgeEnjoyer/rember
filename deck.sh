d96814137196eaeb6fb166dbf28bf7331576068b
KnowledgeEnjoyer <ghsidghisdahfisdhifs@ghdisag.com>
2023-08-23 22:32:58 -0300

    Modularizing and refactoring code.

--- deck.sh
+++ deck.sh
@@ -11,7 +11,7 @@ generate_deck_id() {
 
 create_deck() {
 
-    subject_slug=$(echo "$1" | tr 'A-Z' 'a-z' | sed s/\ /_/g)
+    local subject_slug=$(echo "$1" | tr 'A-Z' 'a-z' | sed s/\ /_/g)
 
 cat <<- EOF > ${REMBER_CONFIG_FOLDER}/${subject_slug}.yaml
 id: $(generate_deck_id)
@@ -20,3 +20,64 @@ cards:
 EOF
 
 }
+
+check_deck_subject() {
+    if [ "$1" != "--subject" ]; then
+        printf "Missing '--subject' option.\n"
+        printf "Example: --subject \"C programming\"\n"
+        return 1
+    fi
+    
+    if [ -z "$2" ]; then
+        printf "Missing string for --subject option.\n"
+        printf "Example: --subject \"C programming\"\n"
+        return 1
+    fi
+
+    return 0
+}
+
+list_decks() {
+    for deck_filename in $(ls "$REMBER_CONFIG_FOLDER")
+    do
+        id=$(yq ".id" "$REMBER_CONFIG_FOLDER/$deck_filename") &&
+        subject=$(yq ".subject" "$REMBER_CONFIG_FOLDER/$deck_filename") &&
+        printf "ID: %d\tDeck Subject: %s\n" "$id" "$subject"
+
+        if [ "$?" -ne 0 ]; then
+            printf "rember: error on listing decks"
+            return 1
+        fi
+
+    done
+
+   return 0
+}
+
+delete_deck() {
+    if [ -z $1 ] || [[ ! $1 =~ ^-?[0-9]+$ ]]; then
+        printf "Is required a valid ID for deleting a deck.\n"
+        printf "Example: 'deck delete 34'\n"
+        return 1
+    fi
+
+    for deck_filename in $(ls "$REMBER_CONFIG_FOLDER")
+    do
+        if [ $1 -eq $(yq ".id" "$REMBER_CONFIG_FOLDER/$deck_filename") ]; then
+            rm "$REMBER_CONFIG_FOLDER/$deck_filename" &&
+
+            if [ "$?" -ne 0 ]; then
+                printf "rember: error deleting deck.\n"
+                return 1
+            fi
+
+            printf "Deck deleted\n"
+        fi
+    done
+
+    printf "rember: deck not found\n"
+
+    return 1    
+}
+
+
--- rember.sh
+++ rember.sh
@@ -45,54 +45,23 @@ if [ "$1" = "deck" ]; then
 
     case "$2" in
         new ) 
-            if [ "$3" != "--subject" ]; then
-                printf "Missing '--subject' option.\n"
-                printf "Example: --subject \"C programming\"\n"
-                exit 1
-            fi
-        
-            if [ -z "$4" ]; then
-                printf "Missing string for --subject option.\n"
-                printf "Example: --subject \"C programming\"\n"
-                exit 1
-            fi
-
-            create_deck "$4"
-
+            check_deck_subject "$3" "$4" &&
+            create_deck "$4" &&
             printf "Deck created: %s\n" "$4"
-            exit 0
+
+            exit "$?"
         ;;
 
         list )
-            for deck_filename in $(ls "$REMBER_CONFIG_FOLDER")
-            do
-                id=$(yq ".id" "$REMBER_CONFIG_FOLDER/$deck_filename")
-                subject=$(yq ".subject" "$REMBER_CONFIG_FOLDER/$deck_filename")
+            list_decks 
 
-                printf "ID: %d\tDeck Subject: %s\n" "$id" "$subject"
-            done
-            exit 0
+            exit "$?"
         ;;
 
         delete ) 
+            delete_deck "$3"
 
-            if [ -z $3 ] || [[ ! $3 =~ ^-?[0-9]+$ ]]; then
-                printf "Is required a valid ID for deleting a deck.\n"
-                printf "Example: 'deck delete 34'\n"
-                exit 1
-            fi
-
-            for deck_filename in $(ls "$REMBER_CONFIG_FOLDER")
-            do
-                if [ $3 -eq $(yq ".id" "$REMBER_CONFIG_FOLDER/$deck_filename") ]; then
-                    rm "$REMBER_CONFIG_FOLDER/$deck_filename"
-                    printf "Deck deleted\n"
-                    exit 0
-                fi
-            done
-
-            printf "Deck not found\n"
-            exit 1
+            exit "$?"
         ;;
 
         study ) ;;
